c4adf39ecf93c04ff3554874ade463a441c5c4d8
Eric Odom <eric@homecareintel.com>
2024-03-11 08:51:58 -0700

    basic clerk install

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { SafeAreaView } from 'react-native';
+import SignInWithOAuth from '@/components/sign-in-oath';
+import * as SecureStore from 'expo-secure-store';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -44,15 +48,48 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+
+const CLERK_PUBLISHABLE_KEY =
+	'pk_test_c3dlZXQtc2NvcnBpb24tOTkuY2xlcmsuYWNjb3VudHMuZGV2JA';
+  
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
+  const tokenCache = {
+		async getToken(key: string) {
+			try {
+				return SecureStore.getItemAsync(key);
+			} catch (err) {
+				return null;
+			}
+		},
+		async saveToken(key: string, value: string) {
+			try {
+				return SecureStore.setItemAsync(key, value);
+			} catch (err) {
+				return;
+			}
+		},
+	};
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <ClerkProvider
+			publishableKey={CLERK_PUBLISHABLE_KEY}
+			tokenCache={tokenCache}
+		>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+				<SignedIn>
+					<Stack>
+						<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+						<Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+					</Stack>
+				</SignedIn>
+				<SignedOut>
+					<SafeAreaView>
+						<SignInWithOAuth />
+					</SafeAreaView>
+				</SignedOut>
+			</ThemeProvider>
+		</ClerkProvider>
   );
 }
